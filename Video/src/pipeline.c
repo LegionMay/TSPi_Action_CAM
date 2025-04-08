c185f7dace0f38a5e24f36a3576d77dc0ea6249e
LegionMay <15379236+legionmay@user.noreply.gitee.com>
2025-04-08 18:55:58 +0800

    Base CAM OK

--- Video/src/pipeline.c
+++ Video/src/pipeline.c
@@ -144,7 +144,7 @@ GstElement* create_pipeline(VideoConfig *config) {
 
     // Set ARGB conversion caps (changed from RGBA to ARGB)
     GstCaps *argb_caps = gst_caps_new_simple("video/x-raw",
-                                             "format", G_TYPE_STRING, "ARGB",
+                                             "format", G_TYPE_STRING, "RGBA",
                                              NULL);
     g_object_set(capsfilter_rgba, "caps", argb_caps, NULL);
     gst_caps_unref(argb_caps);
